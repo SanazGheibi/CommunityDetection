4c62f82331998a56c495d2e5c9dbdd39e681ad78
SanazGheibi <sgheibi@ufl.edu>
2019-10-04 23:06:52 -0400

    Modified to be more automatic

--- CE_Louvain_V5/iterativeRuns.sh
+++ CE_Louvain_V5/iterativeRuns.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-n=$1
+name=$1
+n=$2
 echo -n "" > runtimes.txt
-echo -n "" > allResults.txt
+echo -n "" > "allResults_${name}.txt"
 echo -n "" > bc.txt
-./community ~/Louvain_input/RandomShuffle_amazon.bin -r reordered.bin -c 4 -l -1 -v -q 0.0001 > graph.tree 2> res.txt
+./community ~/Louvain_input/"RandomShuffle_${name}.bin" -r reordered.bin -c 4 -l -1 -v -q 0.0001 > graph.tree 2> res.txt
 grep 'duration' ./res.txt >> runtimes.txt
 grep 'lastLevel' ./res.txt > level_info.txt
 read -r pass level < level_info.txt
 ./hierarchy graph.tree -l $level > comm.txt
-cat res.txt >> allResults.txt
-echo -e "\n" >> allResults.txt
+rm graph.tree
+cat res.txt >> "allResults_${name}.txt"
+echo -e "\n" >> "allResults_${name}.txt"
 
 for((i=1; i < $n; i++)); do
     ./community reordered.bin -p comm.txt -c 4 -l -1 -v -q 0.0001 > graph.tree 2> res.txt
@@ -17,8 +19,9 @@ for((i=1; i < $n; i++)); do
     grep 'lastLevel' ./res.txt > level_info.txt
     read -r pass level < level_info.txt
     ./hierarchy graph.tree -l $level > comm.txt
-    cat res.txt >> allResults.txt
-    echo -e "\n" >> allResults.txt
+    rm graph.tree
+    cat res.txt >> "allResults_${name}.txt"
+    echo -e "\n" >> "allResults_${name}.txt"
 done
 
 echo -e "sum = 0.0\n" >> bc.txt
@@ -28,7 +31,7 @@ do
 done < runtimes.txt
 echo -e "print sum\n" >> bc.txt
 s=`cat bc.txt | bc -l`
-echo "overall runtime: $s"
+echo "overall runtime: $s" >> "allResults_${name}.txt"
 
 #final clean up 
 rm bc.txt
@@ -36,3 +39,4 @@ rm comm.txt
 rm level_info.txt
 rm res.txt
 rm runtimes.txt
+
