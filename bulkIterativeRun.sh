699326a4dbf7429f8726297bfe3661cfcd94c558
SanazGheibi <sgheibi@ufl.edu>
2019-10-13 00:21:35 -0400

    new commit

--- bulkIterativeRun.sh
+++ bulkIterativeRun.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
-for name in amazon dblp IMDB liveJournal
+for name in net6_0.2 net6_0.4 net6_0.6 net6_0.8
 do
 cd ./CE_Louvain_V5
-./iterativeRuns.sh "${name}" 5
+./iterativeRuns.sh "${name}" 
 cd ../CE_Louvain_V5_1
-./iterativeRuns.sh "${name}" 5
+./iterativeRuns.sh "${name}" 
 cd ../CE_Louvain_V5_StructuralPrune
-./iterativeRuns.sh "${name}" 5
+./iterativeRuns.sh "${name}" 
 cd ../Louvain_seq
-./iterativeRuns.sh RandomShuffle "${name}" 5
-./iterativeRuns.sh rcmOrdered "${name}" 5
-./iterativeRuns.sh uscOrdered "${name}" 5
+./iterativeRuns.sh RandomShuffle "${name}" 
+./iterativeRuns.sh rcmOrdered "${name}" 
+./iterativeRuns.sh uscOrdered "${name}"
 cd ../Louvain_updated
-./iterativeRuns.sh "${name}" 5
+./iterativeRuns.sh "${name}" 
 cd ../MoveFastLouvian
-./iterativeRuns.sh "${name}" 5
+./iterativeRuns.sh "${name}" 
 cd ../MoveFast_seq
-./iterativeRuns.sh RandomShuffle "${name}" 5
-./iterativeRuns.sh rcmOrdered "${name}" 5
-./iterativeRuns.sh uscOrdered "${name}" 5
+./iterativeRuns.sh RandomShuffle "${name}"
+./iterativeRuns.sh rcmOrdered "${name}" 
+./iterativeRuns.sh uscOrdered "${name}"
 cd ..
 done
