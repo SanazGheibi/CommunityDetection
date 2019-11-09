c52131a83aecc7f57bd519c78632a98a667fe903
SanazGheibi <sgheibi@ufl.edu>
2019-11-08 21:50:13 -0500

    cache size defined as a variable

--- PreOrder.sh
+++ PreOrder.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-for name in lj #net6_0.2 net6_0.4 net6_0.6 net6_0.8
+cache=1048576
+for name in amazon IMDB liveJournal dblp
 do
 ./RandomShuffle/RSH ./Data/"${name}.txt" > "./RandomShuffle/${name}_log.txt"
 ./Louvain_updated/convert -i ./Data/"${name}_RSH.txt" -o ~/Louvain_input/"RandomShuffle_${name}.bin" -r > "./Louvain_updated/${name}0_log.txt"
 rm ./Data/"${name}.txt"
 ./Gorder-master/Gorder ./Data/"${name}_RSH.txt" > "./Gorder-master/${name}_log.txt"
-./graphReordering-master/block_reordering/br 64 4194304 ./Data/"${name}_RSH_Gorder.txt" ./Data/"${name}_USC.bin" > "./graphReordering-master/block_reordering/${name}_log.txt"
+./graphReordering-master/block_reordering/br 64 ${cache} ./Data/"${name}_RSH_Gorder.txt" ./Data/"${name}_USC.bin" > "./graphReordering-master/block_reordering/${name}_log.txt"
 rm ./Data/"${name}_RSH_Gorder.txt"
 ./USC_to_Louvain/Reformat ./Data/"${name}_USC.bin" ./Data/"${name}_uscLouvainInput.txt" > "./USC_to_Louvain/${name}_log.txt"
 rm ./Data/"${name}_USC.bin"
