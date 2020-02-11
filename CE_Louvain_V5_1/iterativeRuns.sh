ee4ac26b07144f0b1510d033fb1a3304bbefb85f
SanazGheibi <sgheibi@ufl.edu>
2020-02-10 22:02:27 -0500

    modified scripts for debugging

--- CE_Louvain_V5_1/iterativeRuns.sh
+++ CE_Louvain_V5_1/iterativeRuns.sh
@@ -9,24 +9,27 @@ echo "dataset: ${name}"
 echo -n "" > "${path}"/runtimes.txt
 echo -n "" > "${path}"/bc.txt
 "${path}"/community "${dpath}"/Louvain_input/"rcmOrdered_${name}.bin" -c $cache -l -1 -v  -q 0.0001 > "${path}"/graph.tree 2> "${path}"/res.txt
-grep 'duration' "${path}"/res.txt >> "${path}"/runtimes.txt
-grep 'finalModularity' "${path}"/res.txt > "${path}"/mod_info.txt
+grep 'duration' "${path}"/res.txt | tee -a "${path}"/runtimes.txt # >> instead of | tee -a
+grep 'finalModularity' "${path}"/res.txt | tee "${path}"/mod_info.txt # > instead of | tee
 grep 'lastLevel' "${path}"/res.txt > "${path}"/level_info.txt
 read -r pass level < "${path}"/level_info.txt
 "${path}"/hierarchy "${path}"/graph.tree -l $level > "${path}"/comm.txt
 rm "${path}"/graph.tree
 
+echo "*"
+
 stop=0 #run for at least a seond iteration
 while [  ${stop} -lt 1 ]; do
     "${path}"/community "${dpath}"/Louvain_input/"rcmOrdered_${name}.bin" -p "${path}"/comm.txt -c $cache -l -1 -v -q 0.0001 > "${path}"/graph.tree 2> "${path}"/res.txt
-    grep 'duration' "${path}"/res.txt >> "${path}"/runtimes.txt
-    grep 'finalModularity' "${path}"/res.txt > "${path}"/mod_info.txt
+    grep 'duration' "${path}"/res.txt | tee -a "${path}"/runtimes.txt # >> instead of | tee -a
+    grep 'finalModularity' "${path}"/res.txt | tee "${path}"/mod_info.txt # > instead of | tee
     grep 'stopIterating' "${path}"/res.txt > "${path}"/stop_info.txt
     read -r pass stop < "${path}"/stop_info.txt
     grep 'lastLevel' "${path}"/res.txt > "${path}"/level_info.txt
     read -r pass level < "${path}"/level_info.txt
     "${path}"/hierarchy "${path}"/graph.tree -l $level > "${path}"/comm.txt
     rm "${path}"/graph.tree
+    echo "*"
 done
 
 echo -e "sum = 0.0\n" >> "${path}"/bc.txt
@@ -38,7 +41,7 @@ echo -e "print sum\n" >> "${path}"/bc.txt
 s=`cat "${path}"/bc.txt | bc -l`
 echo "overall runtime: $s" 
 cat "${path}"/mod_info.txt
-echo -e "\n"
+echo -e "-----------------------\n"
 
 #final clean up 
 rm "${path}"/bc.txt
